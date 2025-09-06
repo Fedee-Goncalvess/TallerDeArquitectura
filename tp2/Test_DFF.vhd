461de5e283f18ac60c8f82e5835bef5d665c4b7c
Fedee-Goncalves <federico.gon.923@gmail.com>
2025-09-06 01:51:19 -0300

    Practica 2 intentando hacer ejercicio a

--- tp2/Test_DFF.vhd
+++ tp2/Test_DFF.vhd
@@ -16,27 +16,33 @@ begin
     Stimulus: process
     begin
         -- chequeo de preset y clear
-        Preset <= '0'; wait for 5 ns; Preset <= '1'; wait for 5 ns;
-        Clear  <= '0'; wait for 5 ns; Clear  <= '1'; wait for 5 ns;
+        Preset <= '0';                 -- 0 ns
+        wait for 5 ns; Preset <= '1';  -- 5 ns
+
+        wait for 5 ns;                 -- 10 ns
+        Clear  <= '0'; wait for 5 ns;  -- 15 ns
+        Clear  <= '1'; wait for 5 ns;  -- 20 ns
 
         -- interaccion de preset y clear
-        Preset <= '0'; Clear <= '0'; wait for 5 ns;
-        Preset <= '1'; Clear <= '1'; wait for 5 ns;
+        Preset <= '0'; Clear <= '0'; wait for 5 ns; -- 25 ns
+        Preset <= '1'; Clear <= '1'; wait for 5 ns; -- 30 ns
 
         -- limpiar
-        Clear <= '0', '1' after 5 ns; wait for 10 ns;
+        Clear <= '0', '1' after 5 ns; wait for 10 ns; -- 40 ns
 
         -- chequeo de datos y clock
-        Data  <= '1'; Clock <= '0' after 1 ns, '1' after 5 ns; wait for 10 ns;
-        Data  <= '0'; Clock <= '0' after 1 ns, '1' after 5 ns; wait for 10 ns;
+        Data  <= '1'; Clock <= '1' after 5 ns, '0' after 8 ns; wait for 10 ns;
+
+        -- 50 ns
+        Data  <= '0'; Clock <= '1' after 1 ns; wait for 10 ns; -- 60 ns
 
         -- limpiar
-        Clear <= '0', '1' after 5 ns; wait for 10 ns;
+        Clear <= '0', '1' after 5 ns; wait for 10 ns; -- 70 ns
 
         -- interaccion de preset y clock
         Data   <= '0';
         Preset <= '0', '1' after 10 ns;
-        Clock  <= '0', '1' after 5 ns; wait for 10 ns;
+        Clock  <= '0', '1' after 5 ns; wait for 10 ns; -- 80 ns
 
         wait; -- termina la simulación
     end process;

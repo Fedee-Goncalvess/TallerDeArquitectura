f7fef1cf23bf9449b3de3bf22cb93464b7ed4e15
Fedee-Goncalvess <federico.gon.923@gmail.com>
2025-11-02 20:18:55 -0300

    Hasta donde llegue con los ports

--- tp9/Multiplicador4.vhdl
+++ tp9/Multiplicador4.vhdl
@@ -1,23 +1,50 @@
 entity Multiplicador4 is 
     port (
-        A,B: in Bit_Vector(1 downto 0);
+        A,B: in Bit_Vector(3 downto 0);
         CLK,STB: in Bit;
-        Producto: out Bit_Vector(3 downto 0);
-        Done: out Bit;
+        Producto: out Bit_Vector(7 downto 0);
+        Done: out Bit
     );
 end Multiplicador4;
 
 architecture Estructura of Multiplicador4 is
-    
+        signal Init, Shift, Add, Stop : Bit;
+        signal outA, outB : Bit_Vector(7 downto 0);
+
     begin
-        RegA: Latch8 port map (); -- Registro A de 8 bits
+        RegA: ShiftN port map (
+            CLK => CLK, 
+            CLR => '0', 
+            LD => Init, 
+            SH => Shift, 
+            DIR => '0', 
+            D => A, 
+            Q => outA
+        ); -- Registro A de 8 bits
 
-        RegB: Latch8 port map (); -- Registro B de 8 bits
+        RegB: ShiftN port map (
+            CLK => CLK, 
+            CLR => '0', 
+            LD => Init, 
+            SH => Shift, 
+            DIR => '1', 
+            Q => B, 
+            D => outB
+        ); -- Registro B de 8 bits
 
-        Acumulador: Latch8 port map (); -- Registro Acumulador de 8 bit
+        Acumulador: Latch8 port map (
+            D => Sum, 
+            Clk => CLK, 
+            Pre => Add, 
+            Clr => Init, 
+            Q => Acc
+        ); -- Registro Acumulador de 8 bit
 
-        Adder: Adder8 port map (); -- Sumador de 8 bits
+        Adder: Adder8 port map (outB, Acc, Cin, Cout, Sum); -- Sumador de 8 bits
 
-        
- 
+        Stop <= not(outA(7) or outA(6) or outA(5) or outA(4) or outA(3) or outA(2) or outA(1) or outA(0));
+        Controlador: Controller port map (STB, CLK, outA & "00000001", Stop, Init, Shift, Add, Done);
+
+        Producto <= Acc;
+
     end Estructura;
